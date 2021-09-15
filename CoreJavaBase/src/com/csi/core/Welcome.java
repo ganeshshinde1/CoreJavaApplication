e678585de4bf427120f32ae95ec24b61e7e4cd67
ganeshshinde1 <ganeshshinde2681998@gmail.com>
2021-09-15 05:49:56 -0700

    added changes for full stack

--- CoreJavaBase/src/com/csi/core/Welcome.java
+++ CoreJavaBase/src/com/csi/core/Welcome.java
@@ -1,7 +1,7 @@
 package com.csi.core;
-
+// Added by Kiran Jadhav
 public class Welcome {
 	public static void main(String[] args) {
-		System.out.println("WELCOME TO CSI PUNE");
+		System.out.println("WELCOME TO FULL STACK JAVA DEVELOPER PUNE");
 	}
 }
